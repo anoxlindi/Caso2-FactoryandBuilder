37ba882c3d81a5eb78963aba1809300dc88d4c0d
Analia <Analia@LAPTOP-GAILB2PB>
2025-09-28 11:54:51 -0500

    creare una nueva clase solo para interfaz, pense que era solo el consola, aqui estoy nombrando a lo que vamos a usar mis botones y todo , igual le puse con el //

---
+++ src/View/ComboViewInterfaz.java
@@ -0,0 +1,14 @@
+
+package View;
+
+import Controller.ComboController; // Importa el controlador para manejar la lógica del combo
+import javax.swing.*;               // Importa los componentes gráficos de Swing
+import java.awt.*;                  // Importa layouts y estilos visuales
+import java.awt.event.ActionEvent; // Importa eventos de acción (clics)
+import java.awt.event.ActionListener; // Importa el listener para manejar acciones del botón
+
+// Clase que representa la interfaz gráfica del sistema de combos
+public class ComboViewInterfaz extends JFrame { // Hereda de JFrame para crear una ventana
+    private ComboController controller = new ComboController(); // Controlador que coordina la lógica {
+    
+}
